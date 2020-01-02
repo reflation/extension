2979de1c1e64ff141d092b0006792725d8bd4947
Muhun Kim <iam@muhun.kim>
2020-01-01 23:02:12 -0800

    fix typo

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -4,7 +4,7 @@ import { UsernameInput, PasswordInput } from './components/Input'
 import Submit from './components/Submit'
 import { KeepLoginCheckbox } from './components/Checkbox'
 
-import { Box } from '../styles/components/box'
+import { Box } from '../styles/components/Box'
 import { Title } from '../styles/components/Title'
 import { Form } from '../styles/components/Input'
 
