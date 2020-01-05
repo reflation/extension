c74504660db33ffd44c0db896665cdb0eb187a6b
Muhun Kim <iam@muhun.kim>
2020-01-04 17:13:27 -0800

    Implement OptOutUrl

--- src/utils/reconstruct.ts
+++ src/utils/reconstruct.ts
@@ -1,3 +1,30 @@
+import axios from 'axios'
+
+const optOutQueryKey = 'enable'
+
+export const isSessionExpired = async () => {
+  const { data } = await axios.get<string>(
+    'https://dreamy.jejunu.ac.kr/frame/main.do',
+    { headers: { Cookie: document.cookie } }
+  )
+
+  return data.includes('Session 이 종료되었습니다.')
+}
+
+export const isOptOut = () => {
+  return window.location.search.indexOf(`${optOutQueryKey}=false`) !== -1
+}
+
+export const optOutUrl = () => {
+  let query = window.location.search
+  if (query) {
+    query += `&${optOutQueryKey}=false`
+  } else {
+    query = `?${optOutQueryKey}=false`
+  }
+  return window.location.origin + window.location.pathname + query
+}
+
 export const reconstruct = (title: string) => {
   const root = document.createElement('html')
   document.replaceChild(root, document.documentElement)
