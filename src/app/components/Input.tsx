07aaa061d16a6e998f2acb14345757f25c09589a
Muhun Kim <iam@muhun.kim>
2020-01-10 06:06:59 -0800

    Use border UI at first time instead of WarningLabel (#40)

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -1,17 +1,19 @@
-import { h } from 'preact'
+import { h, JSX } from 'preact'
 import { User, Lock } from 'preact-feather'
 
 import { Input as OriginalInput } from '../../styles/components/Input'
-import { darken_ultra, darken_medium } from '../../styles/colors'
+import { darken_ultra, darken_medium, invaild } from '../../styles/colors'
 import { css } from 'linaria'
 import { styled } from 'linaria/react'
 
-const LoginOutside = styled.div`
+const LoginOutside = styled.div<IsWrong & JSX.HTMLAttributes<HTMLDivElement>>`
   display: flex;
   border-radius: 18px;
   padding-top: 5px;
   padding-bottom: 5px;
   background-color: ${darken_ultra};
+  box-shadow: 0 0 0 2px ${props => (props.isWrong ? invaild.outside : '')};
+  border: solid 1px ${props => (props.isWrong ? invaild.inside : '')};
 `
 
 const Icon = css`
@@ -20,19 +22,21 @@ const Icon = css`
   color: ${darken_medium};
 `
 
+type IsWrong = { isWrong: boolean }
+
 const Input = styled(OriginalInput)`
   flex: 9;
 `
 
-export const UsernameInput = () => (
-  <LoginOutside style={{ marginBottom: 16 }}>
+export const UsernameInput = ({ isWrong }: IsWrong) => (
+  <LoginOutside isWrong={isWrong} style={{ marginBottom: 16 }}>
     <User class={Icon} />
     <Input autoFocus name="student_no" placeholder="학번 혹은 교수번호" />
   </LoginOutside>
 )
 
-export const PasswordInput = () => (
-  <LoginOutside>
+export const PasswordInput = ({ isWrong }: IsWrong) => (
+  <LoginOutside isWrong={isWrong}>
     <Lock class={Icon} />
     <Input type="password" name="student_pw" placeholder="비밀번호" />
   </LoginOutside>
--- src/app/components/Label.tsx
+++ src/app/components/Label.tsx
@@ -1,7 +1,4 @@
 import { h } from 'preact'
-import { memo } from 'preact/compat'
-
-import { Result } from '../features/login'
 
 import { red } from '../../styles/colors'
 import { Regular } from '../../styles/components/Text'
@@ -13,12 +10,6 @@ const RedLabel = styled.label`
   padding-bottom: 1rem;
 `
 
-export const WarningLabel = memo(
-  ({ result }: { result: Result.invalid | Result.blocked }) => (
-    <RedLabel>
-      {result === Result.invalid
-        ? '계정 정보가 유효하지 않습니다!'
-        : '6회 이상 틀려 10분간 로그인이 제한됩니다'}
-    </RedLabel>
-  )
+export const WarningLabel = () => (
+  <RedLabel>너무 많이 로그인을 시도하였습니다</RedLabel>
 )
