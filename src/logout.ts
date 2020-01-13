e4dd193772e7026a2688f6f9fbc7d1d4d3ab7bde
Muhun Kim <iam@muhun.kim>
2020-01-13 12:46:33 -0800

    leftFrame select immediately

--- src/logout.ts
+++ src/logout.ts
@@ -1,12 +1,12 @@
+const leftFrame = document.getElementById('leftFrame') as HTMLIFrameElement
+
 const getDocuemntFromIFrame = (frameRef: HTMLIFrameElement) =>
   frameRef.contentWindow
     ? frameRef.contentWindow.document
     : frameRef.contentDocument
 
 setTimeout(() => {
-  getDocuemntFromIFrame(
-    document.getElementById('leftFrame') as HTMLIFrameElement
-  )
+  getDocuemntFromIFrame(leftFrame)
     .querySelector('td[valign="bottom"][align="right"]')
     .addEventListener('click', () => localStorage.clear())
 }, 10000)
