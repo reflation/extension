635651761c8f633b2555a71d4c92148dd53e2d48
Muhun Kim <iam@muhun.kim>
2020-01-15 10:46:01 -0800

    Implement show password

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -1,10 +1,11 @@
 import { h, JSX } from 'preact'
-import { User, Lock } from 'preact-feather'
+import { User, Lock, Eye, EyeOff } from 'preact-feather'
 
 import { Input as OriginalInput } from '../../styles/components/Input'
 import { darken_ultra, darken_medium, invaild } from '../../styles/colors'
 import { css } from 'linaria'
 import { styled } from 'linaria/react'
+import { useState } from 'preact/hooks'
 
 const LoginOutside = styled.div<IsWrong & JSX.HTMLAttributes<HTMLDivElement>>`
   display: flex;
@@ -16,28 +17,53 @@ const LoginOutside = styled.div<IsWrong & JSX.HTMLAttributes<HTMLDivElement>>`
   border: solid 1px ${props => (props.isWrong ? invaild.inside : '')};
 `
 
-const Icon = css`
+const LeftIcon = css`
   flex: 1;
   align-self: center;
   color: ${darken_medium};
 `
 
+const RightIcon = styled.span`
+  flex: 1;
+  align-self: center;
+  cursor: pointer;
+  color: ${darken_medium};
+`
+
+const RightIconInner = css`
+  vertical-align: middle;
+`
+
 type IsWrong = { isWrong: boolean }
 
 const Input = styled(OriginalInput)`
-  flex: 9;
+  flex: 8;
 `
 
 export const UsernameInput = ({ isWrong }: IsWrong) => (
   <LoginOutside isWrong={isWrong} style={{ marginBottom: 16 }}>
-    <User class={Icon} />
+    <User class={LeftIcon} />
     <Input autoFocus name="student_no" placeholder="학번 혹은 교수번호" />
   </LoginOutside>
 )
 
-export const PasswordInput = ({ isWrong }: IsWrong) => (
-  <LoginOutside isWrong={isWrong}>
-    <Lock class={Icon} />
-    <Input type="password" name="student_pw" placeholder="비밀번호" />
-  </LoginOutside>
-)
+type InputType = 'password' | 'text'
+
+export const PasswordInput = ({ isWrong }: IsWrong) => {
+  const [inputType, setInputType] = useState<InputType>('password')
+  const inverse = () =>
+    setInputType(inputType === 'password' ? 'text' : 'password')
+  return (
+    <LoginOutside isWrong={isWrong}>
+      <Lock class={LeftIcon} />
+      <Input type={inputType} name="student_pw" placeholder="비밀번호" />
+      <RightIcon onClick={inverse}>
+        {inputType === 'password' ? (
+          <Eye class={RightIconInner} />
+        ) : (
+          <EyeOff class={RightIconInner} />
+        )}
+      </RightIcon>
+    </LoginOutside>
+  )
+}
