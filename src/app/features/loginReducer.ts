b15749872a62f3d610433f1ae86a5e6b4554f2fa
Muhun Kim <iam@muhun.kim>
2020-02-24 23:52:54 +0900

    Use Thunk Pattern
    Type WIP: This commit contain two type miss match at:
    - src/app/app.tsx:38
    - src/app/features/loginReducer.ts:39

--- src/app/features/loginReducer.ts
+++ src/app/features/loginReducer.ts
@@ -1,21 +1,43 @@
-import { Result } from './login'
+import { Result, TargetElements, requestFromTargetElements } from './login'
 import { useReducer } from 'preact/hooks'
 import { isKeepLoginEnabled } from './localStorage'
 
-export type Action = 'invaild' | 'blocked' | 'keep' | 'unKeep'
+type Action = 'invaild' | 'blocked' | 'keep' | 'unKeep'
 
-export const [INVAILD, BLOCKED, KEEP, UNKEEP] = [
-  'invaild',
-  'blocked',
-  'keep',
-  'unKeep',
-] as const
+const [INVAILD, BLOCKED] = ['invaild', 'blocked'] as const
+export const [KEEP, UNKEEP] = ['keep', 'unKeep'] as const
+
+export const submitCreator = (payload: TargetElements) => async (
+  dispatch: (action: Action) => void
+) => {
+  try {
+    await requestFromTargetElements(payload)
+  } catch (err) {
+    switch (err) {
+      case Result.invalid:
+        dispatch(INVAILD)
+        break
+      case Result.blocked:
+        dispatch(BLOCKED)
+        break
+    }
+  }
+}
 
 type State = { isWrong: Result; isKeepLogin: boolean }
 
-export default () => {
+export default function useThunkReducer() {
   const [state, dispatch] = useReducer<State, Action>(reducer, initialState)
-  return { ...state, dispatch }
+
+  const enhancedDispatch = (action: Action | typeof dispatch) => {
+    if (typeof action === 'function') {
+      // @ts-ignore
+      action(dispatch)
+    } else {
+      dispatch(action)
+    }
+  }
+  return { state, dispatch: enhancedDispatch }
 }
 
 const initialState: State = {
