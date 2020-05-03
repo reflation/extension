dcc219e3333377d9c4aa964f77068960bdc137a2
Muhun Kim <iam@muhun.kim>
2020-05-03 16:09:59 +0900

    Eject jest config to jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  roots: ['<rootDir>/src/app'],
+  preset: 'ts-jest',
+  globals: {
+    'ts-jest': {
+      babelConfig: true,
+    },
+  },
+  moduleNameMapper: {
+    '^react$': 'preact/compat',
+    '^react-dom/test-utils$': 'preact/test-utils',
+    '^react-dom$': 'preact/compat',
+  },
+  setupFilesAfterEnv: ['<rootDir>src/setupTests.ts'],
+}
