b76bfea2f3ade3eb172c6fcf4382add2014c6b54
Muhun Kim <iam@muhun.kim>
2020-05-04 17:40:30 +0900

    Update useReducer test
    I written three test scenarios that submit student account.
    - correct
    - once incorrect
    - five times incorrect

--- src/app/app.test.tsx
+++ src/app/app.test.tsx
@@ -4,23 +4,69 @@ import { mount } from 'enzyme'
 
 jest.mock('./features/login')
 
-import useReducer, { State } from './features/loginReducer'
+import useReducer, {
+  State,
+  submitCreator,
+  ThunkAction,
+} from './features/loginReducer'
 
-import { Result } from './features/login'
+import { Status, TargetElements } from './features/login'
+
+import 'dotenv/config'
+const { expectUsername, expectPassword } = process.env
+
+const mockTargetElementsCreator = (student_no: string, student_pw: string) =>
+  ({
+    student_no: { value: student_no },
+    student_pw: { value: student_pw },
+  } as TargetElements)
 
 describe('initial mounted login reducer', () => {
   let _state: State
+  let _dispatch: (action: ThunkAction) => void
 
   beforeEach(() => {
     function Comp() {
-      const { state } = useReducer()
+      const { state, dispatch } = useReducer()
       _state = state
+      _dispatch = dispatch
       return <main />
     }
     mount(<Comp />)
   })
 
-  test('initial state check', () => {
-    expect(_state).toEqual({ isWrong: Result.clear, isKeepLogin: false })
+  test('Enter correct student account', async done => {
+    const correctStudentAccount = mockTargetElementsCreator(
+      expectUsername,
+      expectPassword
+    )
+    await submitCreator(correctStudentAccount)(_dispatch)
+    expect(_state).toEqual({ status: Status.correct, isKeepLogin: false })
+    done()
+  })
+  describe('Enter incorrect student account', () => {
+    const incorrectStudentAccount = mockTargetElementsCreator(
+      expectUsername,
+      expectPassword + '1'
+    )
+    test('once wrong', async done => {
+      await submitCreator(incorrectStudentAccount)(_dispatch)
+      expect(_state).toEqual({ status: Status.invalid, isKeepLogin: false })
+      done()
+    })
+    test('Login was blocked because it was wrong 5 times', async done => {
+      const incorrectStudentAccount = mockTargetElementsCreator(
+        expectUsername,
+        expectPassword + '1'
+      )
+      await submitCreator(incorrectStudentAccount)(_dispatch)
+      await submitCreator(incorrectStudentAccount)(_dispatch)
+      await submitCreator(incorrectStudentAccount)(_dispatch)
+      await submitCreator(incorrectStudentAccount)(_dispatch)
+      await submitCreator(incorrectStudentAccount)(_dispatch)
+      await submitCreator(incorrectStudentAccount)(_dispatch)
+      expect(_state).toEqual({ status: Status.blocked, isKeepLogin: false })
+      done()
+    })
   })
 })
