ffee6ac17a928878b7a20bbc10054a363291f67a
Muhun Kim <iam@muhun.kim>
2020-05-04 19:41:57 +0900

    Referance preact type to enzyme

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference types="enzyme-adapter-preact-pure" />
 import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-preact-pure'
 
