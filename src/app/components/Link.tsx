ca735841f44209d4271069ea54c90f4ff1ac192e
Muhun Kim <iam@muhun.kim>
2020-10-22 12:31:42 +0900

    Use button instead anchor element
    https://twitter.com/x86chi/status/1318499782332067840

--- src/app/components/Link.tsx
+++
@@ -1,9 +0,0 @@
-import { styled } from 'linaria/react'
-
-import { primary } from '../../styles/colors'
-
-export const Link = styled.a`
-  color: ${primary};
-  font-weight: bold;
-  cursor: pointer;
-`
